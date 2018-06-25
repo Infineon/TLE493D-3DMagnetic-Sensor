7e0264c4feb803dae858af06e2ab7ea42c1c6ad1
Sun Yuxi (IFAG DES SDF FW) <Yuxi.Sun@infineon.com>
2018-06-25 10:30:22 +0200

    prd rigster of a2b6 slightly different -> adapted set rate function

--- src/Tle493d.cpp
+++ src/Tle493d.cpp
@@ -394,3 +394,13 @@ void Tle493d_w2b6::setUpdateRate(uint8_t updateRate){
 	setRegBits(tle493d::PRD, updateRate);
 	tle493d::writeOut(&mInterface, 0x13);
 }
+
+void Tle493d_a2b6::setUpdateRate(uint8_t updateRate){
+	if(updateRate != 0 && updateRate != 1)
+	{
+		updateRate = 1; // slow
+	}
+	// PRD of A2B6 only convers the highest bit
+	setRegBits(tle493d::PRD, updateRate << 2);
+	tle493d::writeOut(&mInterface, 0x13);
+}
