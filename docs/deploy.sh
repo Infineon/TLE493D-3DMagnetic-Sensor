58ba1ab2a4a15ba049754a7156bcaca1fe76abe0
Sun Yuxi (IFAG DES SDF FW) <Yuxi.Sun@infineon.com>
2018-07-03 09:46:28 +0200

    ..

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -5,16 +5,18 @@ PRIVATE_NAME=sherylll
 USR_EMAIL="Yuxi.Sun@infineon.com"
 GH_REPO_REF=github.com/$PRIVATE_NAME/$REPO_NAME.git
 
-# if file exists
-if [ -f "library.pdf" ]; then
-  sudo apt-get install doxygen graphviz
-  #install miktex
-  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D6BC243565B2087BC3F897C9277A7293F59E4889
-  echo "deb http://miktex.org/download/ubuntu xenial universe" | sudo tee /etc/apt/sources.list.d/miktex.list
-  sudo apt-get update
-  sudo apt-get install miktex
-  miktexsetup finish
+sudo apt-get install doxygen graphviz
+#install miktex
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D6BC243565B2087BC3F897C9277A7293F59E4889
+echo "deb http://miktex.org/download/ubuntu xenial universe" | sudo tee /etc/apt/sources.list.d/miktex.list
+sudo apt-get update
+sudo apt-get install miktex
+miktexsetup finish
+
+make 
   
+# if file exists
+if [ -f "library.pdf" ]; then  
   git clone -b docs https://git@$GH_REPO_REF
   cd REPO_NAME
   git config --global push.default simple
